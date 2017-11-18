1c60c0a3d7e821dd5da8c432596f8b9b362f9999
Fausto A. Guerrero <enjoythelive1@gmail.com>
2017-11-17 22:39:55 -0400

    Added linting and building to process

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require('gulp');
+const ts = require("gulp-typescript");
 const Server = require('karma').Server;
+const tslint = require("gulp-tslint");
+const tsProject = ts.createProject("tsconfig.json");
 
 gulp.task('test', function (done) {
     new Server({
@@ -7,3 +10,19 @@ gulp.task('test', function (done) {
         singleRun: true
     }, done).start();
 });
+
+gulp.task("build", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("dist"));
+});
+
+gulp.task("lint", function () {
+    return tsProject.src()
+        .pipe(tslint({
+            formatter: "verbose"
+        }))
+        .pipe(tslint.report());
+});
+
+gulp.task("default", ["lint", "test", "build"]);
