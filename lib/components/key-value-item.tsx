e6fd72f04c4465cf2626a4a75e1a7d00d7ee63a4
Fausto A. Guerrero <enjoythelive1@gmail.com>
2017-11-19 14:24:47 -0400

    Added full functionality (css missing)

--- lib/components/key-value-item.tsx
+++ lib/components/key-value-item.tsx
@@ -3,11 +3,32 @@ import {KeyValue} from "../structures/key-value";
 
 export interface KeyValueItemProps {
     keyValue: KeyValue;
-    keyLabels?: { [prop: string ]: string };
+    elementClass?: string;
 }
+
+const defaultProps = {
+    elementClass: "key-value-item",
+};
 
 export class KeyValueItem extends React.PureComponent<KeyValueItemProps & React.ClassAttributes<KeyValueItem>> {
     public render() {
-        return (<div/>);
+        return (
+            <div className={this.getElementClass()}>
+                <label className="key">{this.getKeyLabel()}:</label>
+                <div className="value">{this.getValue()}</div>
+            </div>
+        );
+    }
+
+    private getElementClass() {
+        return this.props.elementClass || defaultProps.elementClass;
+    }
+
+    private getKeyLabel() {
+        return this.props.keyValue.keyLabel || this.props.keyValue.key;
+    }
+
+    private getValue() {
+        return this.props.keyValue.value;
     }
 }
